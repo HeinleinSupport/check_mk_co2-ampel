903806f9f2a7710976f92fc23c17ae498793281d
Mattias Schlenker <mattias.schlenker@checkmk.com>
2026-04-09 17:22:56 +0200

    proper writing of CO₂

--- _omd_root/local/lib/python3/cmk_addons/plugins/watterott_co2_special/rulesets/ruleset_check_plugins.py
+++ _omd_root/local/lib/python3/cmk_addons/plugins/watterott_co2_special/rulesets/ruleset_check_plugins.py
@@ -78,7 +78,7 @@ def parameter_form_watterott_co2_co2():
 
 rule_spec_watterott_co2_special_humidity = CheckParameters(
     name = "watterott_co2_special_humidity",
-    title = Title("Humidity levels for Watterott CO2 sensor"),
+    title = Title("Humidity levels for Watterott CO₂ sensor"),
     topic = Topic.ENVIRONMENTAL,
     parameter_form = parameter_form_watterott_co2_humidity,
     condition = HostAndItemCondition(item_title=Title("Humidity Watterott sensor")),
@@ -86,7 +86,7 @@ rule_spec_watterott_co2_special_humidity = CheckParameters(
 
 rule_spec_watterott_co2_special_temp = CheckParameters(
     name = "watterott_co2_special_temp",
-    title = Title("Temperature levels for Watterott CO2 sensor"),
+    title = Title("Temperature levels for Watterott CO₂ sensor"),
     topic = Topic.ENVIRONMENTAL,
     parameter_form = parameter_form_watterott_co2_temperature,
     condition = HostAndItemCondition(item_title=Title("Temperature Watterott sensor")),
@@ -94,7 +94,7 @@ rule_spec_watterott_co2_special_temp = CheckParameters(
 
 rule_spec_watterott_co2_special_co2 = CheckParameters(
     name = "watterott_co2_special_co2",
-    title = Title("CO₂ concentration levels for Watterott CO2 sensor"),
+    title = Title("CO₂ concentration levels for Watterott CO₂ sensor"),
     topic = Topic.ENVIRONMENTAL,
     parameter_form = parameter_form_watterott_co2_co2,
     condition = HostAndItemCondition(item_title=Title("CO₂ concentration Watterott sensor")),
