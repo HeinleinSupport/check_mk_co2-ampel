23317f29be55b2ad95938c12891e9db4ad690f18
Robert Sander <r.sander@heinlein-support.de>
2026-04-10 16:36:23 +0200

    replaces replace() with replace_macros()

--- _omd_root/local/lib/python3/cmk_addons/plugins/watterott_co2_special/server_side_calls/special_agent_watterott_co2.py
+++ _omd_root/local/lib/python3/cmk_addons/plugins/watterott_co2_special/server_side_calls/special_agent_watterott_co2.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python3
 # Shebang needed only for editors
 
-from cmk.server_side_calls.v1 import noop_parser, SpecialAgentConfig, SpecialAgentCommand, HostConfig
-import json
+from cmk.server_side_calls.v1 import noop_parser, SpecialAgentConfig, SpecialAgentCommand, HostConfig, replace_macros
 
 # {"outer": ["filesystem", {"file": "/tmp/$HOSTNAME$.txt", "maxage": 180.0}]}
 # {"outer": ["network", {"host": "$HOSTNAME$"}]}
 # The most common ones are $HOSTNAME$, $HOSTALIAS$ or $HOSTADDRESS$.
 
-def command_function(params, host_config):
+def command_function(params, host_config: HostConfig):
     args = []
     if "filesystem" in params["outer"]:
         args.append("--file")
-        args.append(params["outer"][1]["file"].replace("$HOSTNAME$", host_config.name).replace("$HOSTADDRESS$", host_config.ipv4_config.address).replace("$HOSTALIAS$", host_config.alias))
+        args.append(replace_macros(params["outer"][1]["file"], host_config.macros))
         args.append("--maxage")
         args.append(str(params["outer"][1]["maxage"]))
     else:
         args.append("--host")
-        args.append(params["outer"][1]["host"].replace("$HOSTNAME$", host_config.name).replace("$HOSTADDRESS$", host_config.ipv4_config.address).replace("$HOSTALIAS$", host_config.alias))
+        args.append(replace_macros(params["outer"][1]["host"], host_config.macros))
     yield SpecialAgentCommand(command_arguments=args)
 
 special_agent_ometemp = SpecialAgentConfig(
